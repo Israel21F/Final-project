9f700144904be9bdcf3e002c01585b104af8b582
Israel21F <126812093+Israel21F@users.noreply.github.com>
2023-05-23 14:51:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("Script loading");
+// Global variables 
+
+
+
+
+/* When user clicks drop down menu toggle between hiding and showing */
+function myFunction() {
+    console.log(myFunction);
+    console.log();
+
+    
+    }
+    
+   
+    
+
+
+
+function url(){
+    var url = window.location.href
+    var e = document.getElementById("dropDown");
+    var value = e.value;
+    var text = e.options[e.selectedIndex].text;
+    window.location.href = value 
+    
+  
+    console.log(value);
+}
+//When dropDown element is clicked go to link
+
+
+
+
+
+// Close drop down menu when they click outside of it
+
+
+
+
+console.log("Script loaded");
